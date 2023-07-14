b5e6c9f54d0b5ec91803c5bcb8afcb7668c1ba40
Daniel Monteiro <danielmonteirodcarvalho@gmail.com>
2023-07-14 20:36:56 -0300

    Loop for com exemplo de crescente e decrescente

---
+++ LoopFor.py
@@ -0,0 +1,11 @@
+print("Crescente: ")
+for n in range(10):
+    print(n)
+
+print("Crescente com início predeterminado:")
+for n in range(5,16):
+    print(n)
+
+print("Decrescente:")
+for n in range(10, 0, -1):
+    print(n)
